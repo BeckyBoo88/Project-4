b96f09a304b173ac73b25b672915393bc9393903
BeckyBoo88 <rbooco07@gmail.com>
2021-07-27 12:53:59 -0600

    added detail page

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify, redirect
-from werkzeug import datastructures
 from dotenv import load_dotenv
 
 import os
@@ -37,31 +36,7 @@ def park():
     print('🌈', parkCode)
     park = PARK2
     print(park)
-    r = requests.get(f'https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${park}')
+    r = requests.get(f'https://developer.nps.gov/api/v1/parks?parkCode={parkCode}&api_key={park}')
     jsonDets = r.json()
     parkDetails = jsonDets
-    return parkDetails
-
-# @app.route('/cats/<name>')
-# def cat_name(name):
-#     # render template with variable
-#     return render_template('cats_name.html', name=name)
-
-# fake_database = [
-#     'frist the cat',
-#     'snowball',
-#     'sylvester'
-# ]
-
-# # GET/api -- show from fake db
-# @app.route('/api/cats', methods=['GET', 'POST'])
-# def cats_api():
-#     global fake_database
-
-#     if request.method == 'GET':
-#         return jsonify({ 'cats': fake_database })
-#     if request.method == 'POST':
-#         #get request body, for html form
-#         cat = request.form['name']
-#         fake_database.append(cat)
-#         return redirect('/api/cats')
+    return render_template('parkdetails.html', parkDetails = parkDetails)
